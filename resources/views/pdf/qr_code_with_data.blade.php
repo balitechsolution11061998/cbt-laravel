13a59f6fdf4b563a39ae8feb6775af590b1cad73
sulaksana234 <sulaksana60@gmail.com>
2024-08-17 07:54:27 +0800

    update

--- resources/views/pdf/qr_code_with_data.blade.php
+++ resources/views/pdf/qr_code_with_data.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <style>
         body {
@@ -7,6 +8,7 @@
             color: #333;
             margin: 20px;
         }
+
         .container {
             max-width: 800px;
             margin: 0 auto;
@@ -15,10 +17,12 @@
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
+
         h1 {
             text-align: center;
             color: #004d99;
         }
+
         .student-info {
             margin-bottom: 30px;
             padding: 15px;
@@ -26,16 +30,20 @@
             border-radius: 8px;
             background-color: #f9f9f9;
         }
+
         .student-info p {
             margin: 5px 0;
             font-size: 16px;
         }
+
         .student-info strong {
             color: #004d99;
         }
+
         .qr-code {
             text-align: center;
         }
+
         .qr-code img {
             max-width: 150px;
             height: auto;
@@ -46,20 +54,32 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <h1>QR Code for {{ $user->name }}</h1>
+
         <div class="student-info">
             <p><strong>Name:</strong> {{ $user->name }}</p>
-            <p><strong>Class:</strong> {{ $student->rombel->nama_rombel ?? 'N/A' }}</p>
-            <p><strong>NIS:</strong> {{ $student->nis  ?? 'N/A' }}</p>
-            <p><strong>Gender:</strong>
-                {{ $student->jenis_kelamin === 'P' ? 'Perempuan' : 'Laki-laki' }}
-            </p>
+
+            @if ($student)
+                <p><strong>Class:</strong> {{ $student->rombel->nama_rombel ?? 'N/A' }}</p>
+                <p><strong>NIS:</strong> {{ $student->nis ?? 'N/A' }}</p>
+                <p><strong>Gender:</strong>
+                    {{ $student->jenis_kelamin === 'P' ? 'Perempuan' : 'Laki-laki' }}
+                </p>
+            @else
+                <p><strong>Class:</strong> N/A</p>
+                <p><strong>NIS:</strong> N/A</p>
+                <p><strong>Gender:</strong> N/A</p>
+            @endif
         </div>
+
         <div class="qr-code">
             <img src="data:image/png;base64,{{ base64_encode($qrCode) }}" alt="QR Code">
         </div>
     </div>
+
 </body>
+
 </html>
