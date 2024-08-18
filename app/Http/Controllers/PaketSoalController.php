e7e5809349e6e05bb4cd15213e23cc1a54ffc4b9
sulaksana234 <sulaksana60@gmail.com>
2024-08-18 08:04:26 +0800

    update

--- app/Http/Controllers/PaketSoalController.php
+++ app/Http/Controllers/PaketSoalController.php
@@ -6,6 +6,7 @@ use App\Models\PaketSoal;
 use Illuminate\Http\Request;
 use DataTables;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class PaketSoalController extends Controller
 {
@@ -20,15 +21,15 @@ class PaketSoalController extends Controller
 
         return DataTables::of($data)
             ->addIndexColumn()
-            ->addColumn('kelas', function($row) {
+            ->addColumn('kelas', function ($row) {
                 return $row->kelas ? $row->kelas->name : 'N/A';
             })
-            ->addColumn('mata_pelajaran', function($row) {
+            ->addColumn('mata_pelajaran', function ($row) {
                 return $row->mataPelajaran ? $row->mataPelajaran->nama : 'N/A';
             })
-            ->addColumn('action', function($row){
-                $btn = '<a href="javascript:void(0)" data-id="'.$row->id.'" class="edit btn btn-primary btn-sm editPaketSoal" title="Edit"><i class="fas fa-edit"></i></a>';
-                $btn .= ' <a href="javascript:void(0)" data-id="'.$row->id.'" class="delete btn btn-danger btn-sm deletePaketSoal" title="Delete"><i class="fas fa-trash-alt"></i></a>';
+            ->addColumn('action', function ($row) {
+                $btn = '<a href="javascript:void(0)" data-id="' . $row->id . '" class="edit btn btn-primary btn-sm editPaketSoal" title="Edit"><i class="fas fa-edit"></i></a>';
+                $btn .= ' <a href="javascript:void(0)" data-id="' . $row->id . '" class="delete btn btn-danger btn-sm deletePaketSoal" title="Delete"><i class="fas fa-trash-alt"></i></a>';
                 return $btn;
             })
             ->rawColumns(['action'])
@@ -37,7 +38,7 @@ class PaketSoalController extends Controller
     public function dataoptions()
     {
         // Fetch all classes
-        $paketSoal = PaketSoal::select('id','kode_paket')->get();
+        $paketSoal = PaketSoal::select('id', 'kode_paket')->get();
 
         // Return the classes as JSON
         return response()->json($paketSoal);
@@ -65,28 +66,33 @@ class PaketSoalController extends Controller
 
 
     public function update(Request $request, $id)
-{
-    // Find the PaketSoal record by ID
-    $paketSoal = PaketSoal::findOrFail($id);
+    {
+        // Find the PaketSoal record by ID
+        $paketSoal = PaketSoal::findOrFail($id);
 
-    // Validate the incoming request data
-    $validator = Validator::make($request->all(), [
-        'kode_kelas' => 'required|exists:kelas,id',
-        'kode_mata_pelajaran' => 'required|max:255',
-        'kode_paket' => 'required|max:255',
-        'keterangan' => 'nullable|string',
-    ]);
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'kode_kelas' => 'required|exists:kelas,id',
+            'kode_mata_pelajaran' => 'required|max:255',
+            'kode_paket' => [
+                'required',
+                'max:255',
+                Rule::unique('paket_soal')->ignore($paketSoal->id),
+            ],
+            'keterangan' => 'nullable|string',
+        ]);
 
-    if ($validator->fails()) {
-        return response()->json(['errors' => $validator->errors()], 422);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Update the PaketSoal record
+        $paketSoal->update($request->all());
+
+        // Return a success response
+        return response()->json(['success' => 'Paket Soal updated successfully']);
     }
 
-    // Update the PaketSoal record
-    $paketSoal->update($request->all());
-
-    // Return a success response
-    return response()->json(['success' => 'Paket Soal updated successfully']);
-}
 
     public function edit($id)
     {
