8771b74df621c878513cf8b241e458f13efef66d
sulaksana234 <sulaksana60@gmail.com>
2024-09-09 17:16:43 +0800

    update

--- resources/views/ujians/show.blade.php
+++ resources/views/ujians/show.blade.php
@@ -150,7 +150,8 @@
                     </div>
                     <div class="card-body">
                         <p id="question-text">Hasil dari 4 log 8 + 4 log 32 adalah ...</p>
-                        <img id="question-image" src="" alt="Question Image" style="display: none; max-width: 100%; height: auto;">
+                        <img id="question-image" src="" alt="Question Image"
+                            style="display: none; max-width: 100%; height: auto;">
                         <div id="question-options" class="question-options">
                             <!-- Options will be inserted here by JavaScript -->
                         </div>
@@ -187,37 +188,28 @@
         let currentQuestion = 0;
 
         function showQuestion(index) {
+            console.log(questions.length, 'masuk sini');
             currentQuestion = index;
             document.getElementById('question-number').textContent = `Soal No. ${index + 1}`;
             document.getElementById('question-text').textContent = questions[index].pertanyaan;
 
             // Show or hide image based on question type
             const imageElement = document.getElementById('question-image');
-            console.log(questions[index],'masuk sini');
+            console.log(questions[index], 'masuk sini');
 
             if (questions[index].jenis === 'gambar') {
-                imageElement.src = '/'.questions[index].pertanyaan_image; // Set image source
+                imageElement.src = '/' + questions[index].pertanyaan_image; // Set image source
                 imageElement.style.display = 'block'; // Show the image
                 document.getElementById('question-options').innerHTML = ''; // Clear options for image questions
-            } else {
-                imageElement.style.display = 'none'; // Hide the image
-                if (questions[index].jenis === 'essai') {
-                    const answer = answeredQuestions[index] || '';
-                    document.getElementById('question-options').innerHTML = `
-                        <div class="form-group">
-                            <label for="essay-answer">Jawaban:</label>
-                            <textarea class="form-control" id="essay-answer" rows="4" onchange="markAnsweredEssay(${index})">${answer}</textarea>
-                        </div>
-                    `;
-                } else {
-                    const options = [
-                        questions[index].pertanyaan_a,
-                        questions[index].pertanyaan_b,
-                        questions[index].pertanyaan_c,
-                        questions[index].pertanyaan_d
-                    ];
+            }
+            const options = [
+                questions[index].pertanyaan_a,
+                questions[index].pertanyaan_b,
+                questions[index].pertanyaan_c,
+                questions[index].pertanyaan_d
+            ];
 
-                    const optionsHtml = options.map((option, i) => `
+            const optionsHtml = options.map((option, i) => `
                         <div class="form-check">
                             <input class="form-check-input" type="radio" name="question${index}" id="option${String.fromCharCode(65 + i)}" ${answeredQuestions[index] === String.fromCharCode(65 + i) ? 'checked' : ''} onclick="markAnswered(${index}, '${String.fromCharCode(65 + i)}')">
                             <label class="form-check-label" for="option${String.fromCharCode(65 + i)}">
@@ -226,9 +218,8 @@
                         </div>
                     `).join('');
 
-                    document.getElementById('question-options').innerHTML = optionsHtml;
-                }
-            }
+            document.getElementById('question-options').innerHTML = optionsHtml;
+
         }
 
         function markAnsweredEssay(index) {
@@ -262,7 +253,7 @@
 
         function startCountdown(endTime) {
             const countdownElement = document.getElementById('countdown-timer');
-            const countdownInterval = setInterval(function () {
+            const countdownInterval = setInterval(function() {
                 const now = new Date().getTime();
                 const timeRemaining = Math.floor((endTime - now) / 1000);
 
@@ -299,13 +290,16 @@
             } else {
                 const now = new Date().getTime();
                 const endTime = now + (examDuration * 1000); // Calculate end time in milliseconds
-                localStorage.setItem(storageKey, JSON.stringify({ endTime }));
+                localStorage.setItem(storageKey, JSON.stringify({
+                    endTime
+                }));
                 startCountdown(endTime);
             }
 
-            window.addEventListener('beforeunload', function (event) {
+            window.addEventListener('beforeunload', function(event) {
                 event.preventDefault();
-                event.returnValue = 'Ujian belum selesai, apakah Anda yakin ingin meninggalkan halaman ini?';
+                event.returnValue =
+                'Ujian belum selesai, apakah Anda yakin ingin meninggalkan halaman ini?';
                 return 'Ujian belum selesai, apakah Anda yakin ingin meninggalkan halaman ini?';
             });
 
@@ -345,16 +339,20 @@
                             .then(response => response.json())
                             .then(data => {
                                 if (data.success) {
-                                    Swal.fire('Ujian diakhiri!', 'Ujian Anda telah diakhiri.', 'success')
+                                    Swal.fire('Ujian diakhiri!', 'Ujian Anda telah diakhiri.',
+                                            'success')
                                         .then(() => {
-                                            window.location.href = `/ujian/hasil-ujian/${data.hasil_ujian_id}`;
+                                            window.location.href =
+                                                `/ujian/hasil-ujian/${data.hasil_ujian_id}`;
                                         });
                                 } else {
-                                    Swal.fire('Gagal!', 'Terjadi kesalahan saat mengakhiri ujian.', 'error');
+                                    Swal.fire('Gagal!',
+                                        'Terjadi kesalahan saat mengakhiri ujian.', 'error');
                                 }
                             })
                             .catch(error => {
-                                Swal.fire('Gagal!', 'Terjadi kesalahan saat mengakhiri ujian.', 'error');
+                                Swal.fire('Gagal!', 'Terjadi kesalahan saat mengakhiri ujian.',
+                                    'error');
                             });
                     }
                 });
